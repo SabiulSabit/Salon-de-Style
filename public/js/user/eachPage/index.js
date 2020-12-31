e91f7c83de529612fdb1bf10897468e86e2c9cfc
SabitTheBot <iamsabit99@gmail.com>
2021-01-01 02:10:23 +0600

    add index.js

---
+++ public/js/user/eachPage/index.js
@@ -0,0 +1,20 @@
+function validatePassword() {
+    let password = document.getElementById("abc_password")
+    let confirm_password = document.getElementById("confirm_password");
+    console.log(password.value,confirm_password.value)
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+        confirm_password.setCustomValidity('');
+    }
+}
+
+var typed = new Typed('.typed-words', {
+    strings: [" Tattoo shop", " Salon"," Attractions"],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 4000,
+    startDelay: 1000,
+    loop: true,
+    showCursor: true
+});
