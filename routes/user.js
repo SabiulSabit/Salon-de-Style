a44884e719e2acd7aa00f5a68f164c800d6f8d52
SabitTheBot <iamsabit99@gmail.com>
2021-01-23 19:49:27 +0600

    update authentication routing

--- routes/user.js
+++ routes/user.js
@@ -6,104 +6,105 @@ const router = express.Router();
 
 
 const userController = require('../controllers/user/user');
+const userAuthentication = require('../controllers/user/authentication');
 
 
 router.route('/')
-      .get(userController.setLocals, userController.getHome);
+      .get(userAuthentication.setLocals, userController.getHome);
 
  
 //show shop info
 router.route('/details/:name/:mail')
-      .get(userController.isAuthentic1,userController.setLocals,userController.getShowShop);
+      .get(userAuthentication.isAuthentic1,userAuthentication.setLocals,userController.getShowShop);
 
 //login 
 router.route('/login')      
-      .post(userController.setLocals,userController.postLogin);
+      .post(userAuthentication.setLocals,userAuthentication.postLogin);
 
 //create account
 router.route('/createaccount')
-      .post(userController.setLocals,userController.postCreateAccount) 
+      .post(userAuthentication.setLocals,userAuthentication.postCreateAccount) 
 
 ///cart      
 router.route('/cart')
-      .post(userController.isAuthentic,userController.setLocals,userController.getCart)      
+      .post(userAuthentication.isAuthentic,userAuthentication.setLocals,userController.getCart)      
 
 //Service
 router.route('/service/:name/:catName/:price')
-      .get(userController.isAuthentic,userController.setLocals,userController.setLocals,userController.getService)
+      .get(userAuthentication.isAuthentic,userAuthentication.setLocals,userController.getService)
 
 //Check out     
 router.route('/checkout')
-      .post(userController.isAuthentic,userController.setLocals, userController.postCheckOut)
+      .post(userAuthentication.isAuthentic,userAuthentication.setLocals, userController.postCheckOut)
 
 //Payment 
 router.route('/payment')
-      .get(userController.isAuthentic,userController.setLocals,userController.getPayment)  
+      .get(userAuthentication.isAuthentic,userAuthentication.setLocals,userController.getPayment)  
       
 
 //Profile   
 router.route('/profile')
-      .get(userController.setLocals,userController.isAuthentic,userController.getProfile)    
+      .get(userAuthentication.setLocals,userAuthentication.isAuthentic,userController.getProfile)    
       
 router.route('/updateprofile')
-      .post(userController.isAuthentic,userController.setLocals,userController.updateProfile)     
+      .post(userAuthentication.isAuthentic,userAuthentication.setLocals,userController.updateProfile)     
       
  ///chnagepassword   
  router.route('/chnagepassword')
-      .post(userController.isAuthentic,userController.setLocals,userController.chnagePassword)       
+      .post(userAuthentication.isAuthentic,userAuthentication.setLocals,userController.chnagePassword)       
 
 //report
 router.route('/report/:shopmail')
-      .post(userController.isAuthentic,userController.setLocals,userController.postReport)      
+      .post(userAuthentication.isAuthentic,userAuthentication.setLocals,userController.postReport)      
     
 router.route('/order/package/:token')
-      .post(userController.isAuthentic,userController.postOrderPackage)
+      .post(userAuthentication.isAuthentic,userController.postOrderPackage)
 
 //dashbord
 router.route('/dashbord')
-      .get(userController.isAuthentic,userController.setLocals,userController.getDashbord)
+      .get(userAuthentication.isAuthentic,userAuthentication.setLocals,userController.getDashbord)
 
 ///egiftcard
 router.route('/egiftcard')
-      .get(userController.isAuthentic,userController.setLocals,userController.getEGiftCard)
+      .get(userAuthentication.isAuthentic,userAuthentication.setLocals,userController.getEGiftCard)
 
 ////appointment
 router.route('/appointment')
-      .get(userController.isAuthentic,userController.setLocals,userController.getAppointment)
+      .get(userAuthentication.isAuthentic,userAuthentication.setLocals,userController.getAppointment)
   
 ////review&favourites      
 router.route('/review&favourites')
-      .get(userController.isAuthentic,userController.setLocals,userController.getReviewFavourites)
+      .get(userAuthentication.isAuthentic,userAuthentication.setLocals,userController.getReviewFavourites)
 
 //terms&services
 router.route('/terms&services')
-      .get(userController.isAuthentic,userController.setLocals,userController.getTermsServices)     
+      .get(userAuthentication.isAuthentic,userAuthentication.setLocals,userController.getTermsServices)     
 
 ///privacy
 router.route('/privacy')
-      .get(userController.isAuthentic,userController.setLocals,userController.getPrivacy)    
+      .get(userAuthentication.isAuthentic,userAuthentication.setLocals,userController.getPrivacy)    
 
 ///setReview      
 router.route('/setReview')
-      .post(userController.isAuthentic,userController.setLocals,userController.postReview)   
+      .post(userAuthentication.isAuthentic,userAuthentication.setLocals,userController.postReview)   
 
 ///delete/appoinment
 router.route('/delete/appoinment')
-      .post(userController.isAuthentic,userController.setLocals,userController.postDeleteAppoinment)
+      .post(userAuthentication.isAuthentic,userAuthentication.setLocals,userController.postDeleteAppoinment)
 
 
 // /search
 router.route('/search')
-      .post(userController.setLocals,userController.postSearch)
+      .post(userAuthentication.setLocals,userController.postSearch)
 
 ///search/category/
 router.route('/search/category/:name')
-      .get(userController.setLocals,userController.getSearchByCategory)     
+      .get(userAuthentication.setLocals,userController.getSearchByCategory)     
           
 
 ///logout
 router.route('/logout')      
-      .get(userController.getLogout);
+      .get(userAuthentication.getLogout);
 
  //export
 module.exports = router;     
